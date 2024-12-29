687ece1be922355db5dda4e2e590cf57e371271d
IvoKara <ivo.i.karaneshev@gmail.com>
2024-12-29 17:18:57 +0200

    fix: remove status column on reverse migrations

--- database/migrations/2024_12_29_145052_add_course_status_to_courses_table.php
+++ database/migrations/2024_12_29_145052_add_course_status_to_courses_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            //
+            $table->dropColumn('status');
         });
     }
 };
