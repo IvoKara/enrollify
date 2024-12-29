3e09c2ef91c85a6c2b7cbde5ee0ad4f4869ecc10
IvoKara <ivo.i.karaneshev@gmail.com>
2024-12-29 14:52:48 +0200

    chore: update fillable fields for Course model

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Course extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'slug',
+        'description',
+        'duration',
+        'meta_description',
+    ];
 }
