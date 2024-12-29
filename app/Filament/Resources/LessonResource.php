af33274982de9af0d9ac70217b5a4f824b5fc401
IvoKara <ivo.i.karaneshev@gmail.com>
2024-12-29 20:02:17 +0200

    feat: create lesson filament resouse and provide create / edit abilities

---
+++ app/Filament/Resources/LessonResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LessonResource\Pages;
+use App\Models\Lesson;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+
+class LessonResource extends Resource
+{
+    protected static ?string $model = Lesson::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make()->schema([
+                    TextInput::make('title')
+                        ->required()
+                        ->label('Title'),
+                    TextInput::make('meta_description')
+                        ->required()
+                        ->label('Meta Description'),
+                    TextInput::make('duration')
+                        ->integer()
+                        ->required()
+                        ->label('Duration'),
+                    RichEditor::make('overview')
+                        ->required()
+                        ->label('Overview'),
+                    Checkbox::make('is_free'),
+                    Select::make('course_id')
+                        ->relationship('course', 'title')
+                        ->searchable()
+                        ->required(),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLessons::route('/'),
+            'create' => Pages\CreateLesson::route('/create'),
+            'edit' => Pages\EditLesson::route('/{record}/edit'),
+        ];
+    }
+}
