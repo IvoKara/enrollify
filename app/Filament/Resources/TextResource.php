45b046e85c3cf28e425a552b64d5eaca283b2c4c
IvoKara <ivo.i.karaneshev@gmail.com>
2024-12-30 00:04:45 +0200

    feat: create / edit text resource

---
+++ app/Filament/Resources/TextResource.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TextResource\Pages;
+use App\Models\Text;
+use Awcodes\Curator\Components\Forms\CuratorPicker;
+use Awcodes\Curator\Components\Tables\CuratorColumn;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class TextResource extends Resource
+{
+    protected static ?string $model = Text::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                CuratorPicker::make('media_id')->label('Image'),
+                RichEditor::make('content')->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                CuratorColumn::make('media_id')
+                    ->label('Image')
+                    ->size(60)
+                    ->ring(2),
+
+                TextColumn::make('content')
+                    ->html()
+                    ->words(10)
+                    ->lineClamp(3)
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTexts::route('/'),
+            'create' => Pages\CreateText::route('/create'),
+            'edit' => Pages\EditText::route('/{record}/edit'),
+        ];
+    }
+}
